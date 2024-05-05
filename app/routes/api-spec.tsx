92a132c609f05ecd0a69afb2307534514896b608
Vick Khera <vivek@khera.org>
2024-05-05 13:08:05 -0400

    remove API spec rendering
    The @scalar/api-reference-react package pulls in over 200 other packages, so remove it for now.

--- app/routes/api-spec.tsx
+++ app/routes/api-spec.tsx
@@ -1,3 +1,4 @@
+/*
 import { ApiReferenceReact } from '@scalar/api-reference-react'
 import apiSpec from '~/lib/generated/PublicAPI.json';
 
@@ -14,4 +15,31 @@ export default function ApiSpec() {
       />
     </>
   );
+}
+*/
+
+import { List, ListItem, ListItemText, ListItemIcon, Typography } from '@mui/material';
+import {LooksOneOutlined, LooksTwoOutlined} from '@mui/icons-material';
+
+export default function ApiSpec() {
+  return (
+    <>
+      <Typography variant="h2">API Spec Placeholder</Typography>
+      <Typography variant="body1">The package to render the spec is gigantic (about 600 pacakges!), and it really is not needed for this toy API. To enable it perform the following steps:</Typography>
+      <List>
+        <ListItem>
+          <ListItemIcon><LooksOneOutlined /></ListItemIcon>
+          <ListItemText>
+            Install the package with <Typography variant="caption">npm install @scalar/api-reference-react</Typography>
+          </ListItemText>
+        </ListItem>
+        <ListItem>
+          <ListItemIcon><LooksTwoOutlined /></ListItemIcon>
+          <ListItemText>
+            Uncomment the commented out bits of this route handler and comment out the uncommented bits.
+          </ListItemText>
+        </ListItem>
+      </List>
+    </>
+  )
 }
